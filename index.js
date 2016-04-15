9c208cfaef8f0018d667c45b9c4e8ccda6dfdaf9
Josep Subils <jsubils@anpro21.com>
2016-04-15 13:24:54 +0200

    update: only one jar

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var spawn = require('child_process').spawn;
 
 module.exports = function (url, proxy, callback) {
-  var query = spawn('java', ["-classpath", __dirname + "/boiler_pipe/dist/boilerpipe-1.2.0.jar:" + __dirname + "/boiler_pipe/dist/:" + __dirname + "/boiler_pipe/lib/nekohtml-1.9.13.jar:" + __dirname + "/boiler_pipe/lib/xerces-2.9.1.jar", "de.l3s.boilerpipe.demo.Oneliner", url, proxy]);
+  var query = spawn('java', ["-jar", __dirname + "/boilerpipe.jar", url, proxy]);
     var text = ""
     var err = null
     query.stdout.setEncoding('utf8')
