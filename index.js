91d252a0553801eb1d082b965772d32fdefc3a93
Josep Subils <jsubils@anpro21.cm>
2016-08-04 09:58:26 +0200

    0.0.25: update exec child_process

--- index.js
+++ index.js
@@ -1,30 +1,17 @@
 var exec = require('child_process').exec;
 
 module.exports = function (url, proxy, callback) {
-  var proc;
-  if (typeof proxy === 'string') proc = exec('java -jar ' + __dirname + '/boilerpipe.jar ' + url + ' \"' + proxy + '\"');
-  else {
-    callback = proxy;
-    proc = exec('java -jar ' + __dirname + '/boilerpipe.jar ' + ' \"' + url + '\"');
-  }
-
-  var timeout = setTimeout(function () {
-    callback("KILL", "");
-    proc.kill('SIGHUP');
-  }, 60000);
-  var list = [];
-  var error = [];
-  proc.stdout.setEncoding('utf8');
-  proc.stdout.on('data', function (res) {
-    list.push(res);
-  });
-
-  proc.stderr.on('error', function (err) {
-    error.push(err);
-  });
-
-  proc.stdout.on('end', function () {
-    clearTimeout(timeout);
-    callback(error.join(""), list.join(""));
-  });
+	var proc = '';
+	if (typeof proxy === 'string') proc = 'java -jar ' + __dirname + '/boilerpipe.jar ' + url + ' \"' + proxy + '\"';
+	else {
+		callback = proxy;
+		proc = 'java -jar ' + __dirname + '/boilerpipe.jar ' + ' \"' + url + '\"';
+	}
+	exec(proc, { timeout : 60000 }, function (error, stdout, stderr) {
+		if (error) callback(error);
+		else {
+			console.err(stdout);
+			callback(null, stdout);
+		}
+	});
 };
