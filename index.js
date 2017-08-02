900d12b7f91fd2e1a1b087a00da81157714d072a
Josep Subils <josep@spellbook.io>
2017-08-02 15:26:21 +0200

    [update] es6 syntax

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var exec = require('child_process').exec;
+const exec = require('child_process').exec
 
-module.exports = function (url, proxy, callback) {
-	var proc = '';
-	if (typeof proxy === 'string') proc = 'java -jar ' + __dirname + '/boilerpipe.jar ' + url + ' \"' + proxy + '\"';
-	else {
-		callback = proxy;
-		proc = 'java -jar ' + __dirname + '/boilerpipe.jar ' + ' \"' + url + '\"';
-	}
-	exec(proc, { timeout : 60000 }, function (error, stdout, stderr) {
-		if (error) callback(error);
-		else callback(null, stdout);
-	});
-};
+module.exports = (url, proxy, callback) => {
+  var proc
+  if (typeof proxy === 'string') proc = `java -jar ${__dirname}/boilerpipe.jar ${url} "${proxy}"`
+  else {
+    callback = proxy
+    proc = `java -jar ${__dirname}/boilerpipe.jar "${url}"`
+  }
+  exec(proc, { timeout : 60000 }, (error, stdout, stderr) => {
+    if (error) callback(error)
+    else callback(null, stdout)
+  })
+}
